a53acb9d73f52843e6225562351e67ad2982760c
Truc Quynh Dang <dtrquynhey@gmail.com>
2023-12-06 04:07:15 -0500

    before testing adding new staff

--- app/src/main/java/controllers/staff/NewStaffFragment.java
+++ app/src/main/java/controllers/staff/NewStaffFragment.java
@@ -1,59 +1,118 @@
 package controllers.staff;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.superhoodcleaning.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.UUID;
 
 import models.Staff;
 import services.FirebaseConnection;
 import services.IAddButton;
 
-public class NewStaffFragment extends Fragment implements IAddButton {
+public class NewStaffFragment extends Fragment implements IAddButton, View.OnClickListener, OnSuccessListener, OnFailureListener
+{
+
+    private EditText edFirstNameNewStaff, edLastNameNewStaff, edEmailNewStaff, edPhoneNewStaff;
+    private ImageView imageViewPhoto;
+    private Button btnUploadPhotoNewStaff, btnRemovePhotoNewStaff;
+    Uri photoPath;
+    ProgressDialog progressDialog;
+    private String uploadedPhotoUrl = null;
+
+    StorageReference storageReference, imagesStorageRef;
+
 
     ActivityResultLauncher activityResultLauncher;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        btnAdd();
-        initialize();
         return inflater.inflate(R.layout.fragment_new_staff, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initialize(view);
+    }
 
-    private void initialize() {
+    private void initialize(View view) {
+        edFirstNameNewStaff = view.findViewById(R.id.edFirstNameNewStaff);
+        edLastNameNewStaff = view.findViewById(R.id.edLastNameNewStaff);
+        edEmailNewStaff = view.findViewById(R.id.edEmailNewStaff);
+        edPhoneNewStaff = view.findViewById(R.id.edPhoneNewStaff);
+
+        btnUploadPhotoNewStaff = view.findViewById(R.id.btnUploadPhotoNewStaff);
+        btnRemovePhotoNewStaff = view.findViewById(R.id.btnRemovePhotoNewStaff);
+
+        imageViewPhoto = view.findViewById(R.id.imageViewPhoto);
+        btnUploadPhotoNewStaff.setOnClickListener(this);
+        btnRemovePhotoNewStaff.setOnClickListener(this);
+
         activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
-//                         getPhoto(result);
+                        Log.d("ActivityResultDebug", "onActivityResult called with result code: " + result.getResultCode());
+                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                            Log.d("ActivityResultDebug", result.toString());
+                            Bitmap bitmap = convertPhotoResultToBitMap(result);
+                            imageViewPhoto.setImageBitmap(bitmap);
+                        } else {
+                            Log.d("ActivityResultDebug", "Result not OK!");
+                        }
                     }
                 }
         );
     }
-    private void btnAdd() {
-        String firstName = "Haitham";
-        String lastName = "Elkalmoushy";
-        String email = "haitham@gmail.com";
-        String phone = "4389991010";
-        String photoUrl = "https://firebasestorage.googleapis.com/v0/b/superhoodcleaningfirebase.appspot.com/o/images%2Fhaitham.png?alt=media&token=b634fbac-2269-47a9-8b9a-60becbf4feac";
 
-        addStaff(firstName, lastName, email, phone, photoUrl);
+    private Bitmap convertPhotoResultToBitMap(ActivityResult result) {
+        Bitmap bitmap = null;
+        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+            photoPath = result.getData().getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoPath);
+                Log.d("bitmap", bitmap.toString());
+            } catch (Exception e) {
+                Log.d("ADV_FIREBASE", e.getMessage());
+            }
+        }
+        else {
+            Toast.makeText(getContext(), "No result!", Toast.LENGTH_LONG).show();
+        }
+        return bitmap;
     }
 
     private void addStaff(String firstName, String lastName, String email, String phone, String photoUrl) {
@@ -71,16 +130,82 @@ public class NewStaffFragment extends Fragment implements IAddButton {
             Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
-    
-    private void UploadPhoto() {
+
+    private void browseAndChoosePhoto() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        activityResultLauncher.launch(Intent.createChooser(intent,"select image"));
+        activityResultLauncher.launch(Intent.createChooser(intent,"Select image"));
+    }
+
+    private void uploadChosenPhotoToStorage() {
+        if (photoPath != null) {
+            progressDialog = new ProgressDialog(getContext());
+            progressDialog.setTitle("Uploading photo in progress...");
+            progressDialog.show();
+
+            imagesStorageRef = storageReference.child("images/" + UUID.randomUUID());
+            imagesStorageRef.putFile(photoPath).addOnSuccessListener(this).addOnFailureListener(this);
+        }
     }
 
     @Override
     public void addButton() {
         Log.d("MyAppTag", "btn staff clicked");
+        String firstName = edFirstNameNewStaff.getText().toString().trim();
+        String lastName = edLastNameNewStaff.getText().toString().trim();
+        String email = edEmailNewStaff.getText().toString().trim();
+        String phone = edPhoneNewStaff.getText().toString().trim();
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty()) {
+            Toast.makeText(getContext(), "Please fill in all the fields.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (photoPath != null) {
+            uploadChosenPhotoToStorage();
+        } else {
+            // Handle the case when there is no photo selected
+            addStaff(firstName, lastName, email, phone, null);
+        }
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnUploadPhotoNewStaff) {
+            Log.d("MyAppTag", "btn upload photo clicked");
+            browseAndChoosePhoto();
+        } else if (v.getId() == R.id.btnRemovePhotoNewStaff) {
+
+        }
+    }
+    private void onPhotoUploadComplete() {
+        if (uploadedPhotoUrl != null) {
+            String firstName = edFirstNameNewStaff.getText().toString().trim();
+            String lastName = edLastNameNewStaff.getText().toString().trim();
+            String email = edEmailNewStaff.getText().toString().trim();
+            String phone = edPhoneNewStaff.getText().toString().trim();
+            addStaff(firstName, lastName, email, phone, uploadedPhotoUrl);
+        }
+    }
+
+    @Override
+    public void onSuccess(Object o) {
+        Toast.makeText(getContext(), "Photo has been successfully uploaded!", Toast.LENGTH_LONG).show();
+        progressDialog.dismiss();
+        imagesStorageRef.getDownloadUrl().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                uploadedPhotoUrl = task.getResult().toString();
+                onPhotoUploadComplete();
+            } else {
+                Toast.makeText(getContext(), "Failed to get photo URL.", Toast.LENGTH_LONG).show();
+            }
+        });    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        Toast.makeText(getContext(), "Error occurred while uploading photo: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        progressDialog.dismiss();
+    }
+
 }
