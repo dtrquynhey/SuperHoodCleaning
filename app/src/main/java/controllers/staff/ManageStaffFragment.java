803bf01d2932ba5a2ba1bea3ffdd37cb2361924a
Ajinkya Bhintade <40215362+Ajinkyabhintade91@users.noreply.github.com>
2023-12-07 00:13:23 -0500

    Created Grid view inside Manage Staff along with the views and adapter

--- app/src/main/java/controllers/staff/ManageStaffFragment.java
+++ app/src/main/java/controllers/staff/ManageStaffFragment.java
@@ -2,6 +2,8 @@ package controllers.staff;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.superhoodcleaning.R;
 
@@ -16,24 +19,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import models.Staff;
+import services.GridAdapter;
 
 public class ManageStaffFragment extends Fragment {
 
-    private List<Staff>staffList;
+    private List<Staff> staffList;
+
+    String names[] = {"Sizar Haj Hasan", "Truc Dang Quynh", "Ajinkya Bhintade", "Haitham Elkalmoushy"};
+    String imgURL[] = {};
+    GridView gridView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_manage_staff,container,false);
-        staffList = getSampleData();
-        RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
-        GridLayoutManager layoutManager = new GridLayoutManager(requireContext(),2);
-        recyclerView.setLayoutManager(layoutManager);
-        Staff staff = new Staff(staffList);
+        View view = inflater.inflate(R.layout.fragment_manage_staff,container,false);
+//        staffList = getSampleData();
+//        RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
+//        GridLayoutManager layoutManager = new GridLayoutManager(requireContext(),2);
+//        recyclerView.setLayoutManager(layoutManager);
+//        Staff staff = new Staff(staffList);
         //recyclerView.setAdapter(staff);
         // Inflate the layout for this fragment
-        return rootView;
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        gridView = view.findViewById(R.id.gvStaff);
+        gridView.setAdapter(new GridAdapter(names, imgURL,requireContext()));
+    }
+
     private List<Staff> getSampleData() {
         List<Staff> dataList = new ArrayList<>();
 
---
+++ app/src/main/java/services/GridAdapter.java
@@ -0,0 +1,62 @@
+package services;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.superhoodcleaning.R;
+
+public class GridAdapter extends BaseAdapter {
+
+    private final String[] staffName;
+    private final String[] imgURL;
+    Context context;
+    View view;
+    LayoutInflater inflater;
+
+    public GridAdapter(String[] staffName, String[] imgURL, Context context) {
+        this.staffName = staffName;
+        this.imgURL = imgURL;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return staffName.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (inflater == null) {
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        if (convertView == null) {
+            view = new View(context);
+            view = inflater.inflate(R.layout.grid_item_layout, parent, false);
+            ImageView imageView = view.findViewById(R.id.ivAvatar);
+            TextView textView = view.findViewById(R.id.tvStaffName);
+
+            Glide.with(context).load(imgURL[position]).into(imageView);
+            textView.setText(staffName[position]);
+        }
+
+//        Staff staff =
+        return view;
+    }
+}
