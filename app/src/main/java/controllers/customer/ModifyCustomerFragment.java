3afe072a240dcdb75ea2531d07ad97cad64ce6be
Sizar Haj Hasan <hajhasansizar11@gmail.com>
2023-12-08 00:34:25 -0500

    update and delete customer is done

--- app/src/main/java/controllers/customer/ModifyCustomerFragment.java
+++ app/src/main/java/controllers/customer/ModifyCustomerFragment.java
@@ -1,5 +1,7 @@
 package controllers.customer;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.superhoodcleaning.R;
@@ -16,14 +19,16 @@ import com.google.firebase.database.DatabaseReference;
 
 import java.io.Serializable;
 
+import controllers.TabBarActivity;
 import models.Address;
 import models.Customer;
 import services.FirebaseConnection;
 
 public class ModifyCustomerFragment extends Fragment {
-    private EditText edNameModifyCustomer, edManagerModifyCustomer, edStreetModifyCustomer,
+     EditText edNameModifyCustomer, edManagerModifyCustomer, edStreetModifyCustomer,
                     edCityModifyCustomer, edZipModifyCustomer, edPhoneModifyCustomer ;
-    private Button viewAppointment, btnUpdate, btnDelete;
+    Spinner spState;
+     Button viewAppointment, btnUpdate, btnDelete;
 
     Customer customerToUpdate;
     public static ModifyCustomerFragment newInstance(){
@@ -49,13 +54,27 @@ public class ModifyCustomerFragment extends Fragment {
         edManagerModifyCustomer = view.findViewById(R.id.edManagerModifyCustomer);
         edStreetModifyCustomer = view.findViewById(R.id.edStreetModifyCustomer);
         edCityModifyCustomer = view.findViewById(R.id.edCityModifyCustomer);
+        spState = view.findViewById(R.id.spStateModifyCustomer);
         edZipModifyCustomer = view.findViewById(R.id.edZipModifyCustomer);
         edPhoneModifyCustomer = view.findViewById(R.id.edPhoneModifyCustomer);
         viewAppointment = view.findViewById(R.id.viewAppointment);
+
         btnUpdate = view.findViewById(R.id.btnUpdate);
         btnDelete = view.findViewById(R.id.btnDelete);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnUpdateClicked();
+            }
+        });
 
-        Toast.makeText(getContext(), "The customer name is " + customerToUpdate.getName(), Toast.LENGTH_SHORT).show();
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDeleteConfirmationDialog();
+            }
+        });
+
         //Set customer info
         edNameModifyCustomer.setText(customerToUpdate.getName());
         edManagerModifyCustomer.setText(customerToUpdate.getManager());
@@ -70,26 +89,53 @@ public class ModifyCustomerFragment extends Fragment {
 
     }
 
-    private void btnUpdate() {
-        String name = "Chipotle Mexican Grill";
-        String manager = "Truc Quynh Dang";
-        String street = "200 Consumer Square Ste 208";
-        String city = "Plattsburgh";
-        String state = "NY";
-        String zip = "12901";
-        String phone = "4383375346";
+
+    private void btnUpdateClicked() {
+        String name = edNameModifyCustomer.getText().toString();
+        String manager = edManagerModifyCustomer.getText().toString();
+        String street = edStreetModifyCustomer.getText().toString();
+        String city = edCityModifyCustomer.getText().toString();
+        String state = spState.getSelectedItem().toString();
+        String zip = edZipModifyCustomer.getText().toString();
+        String phone = edPhoneModifyCustomer.getText().toString();
         Address address = new Address(street, city, state, zip);
         Customer customer = new Customer(name, manager, address, phone);
 
-        updateCustomer("-NjP_URyryhUQhUPsP8r", customer);
-
+        updateCustomer(customerToUpdate.getCustomerId(), customer);
+        backToManageCustomers();
     }
 
-    private void btnDelete() {
-        deleteCustomer("-NjP_URyryhUQhUPsP8r");
+    private void showDeleteConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Delete Customer");
+        builder.setMessage("Are you sure you want to delete " + customerToUpdate.getName() + " ?");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // User clicked the "Delete" button, so delete the item.
+                deleteCustomer(customerToUpdate.getCustomerId());
+                backToManageCustomers();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
-    //--------------------------------------------------------------------TESTING WITH MOCK DATA
+
     private void updateCustomer(String customerId, Customer customer) {
         try {
             DatabaseReference dbRef = FirebaseConnection.getDatabaseRef().child("customers").child(customerId);
@@ -124,8 +170,11 @@ public class ModifyCustomerFragment extends Fragment {
         }
     }
 
-    private void loadCustomer(Customer customer) {
-
+    private void backToManageCustomers() {
+        if (getActivity() instanceof TabBarActivity) {
+            ((TabBarActivity) getActivity()).replaceFragmentWith(new ManageCustomerFragment(), null);
+        }
     }
+
 
 }
